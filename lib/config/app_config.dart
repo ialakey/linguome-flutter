a948d6abc01e30418c6783dec8fc17d545de00bb
Ilia <56916175+ialakey@users.noreply.github.com>
2025-04-25 00:20:32 +0200

    Update app_config.dart

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,8 +1,8 @@
 class AppConfig {
-  static const String baseUrlApi = 'https://api.linguo.me';
-  static const String googleClientId = '986144635475-8pufine1af78e723vo8ggc2vobk278c5.apps.googleusercontent.com';
-  static const String appleClientId = '986144635475-999qqpe63qae77qrsqs11d4skcqj7ns4.apps.googleusercontent.com';
-  static const String authAppleClientId = '8YSAG6RNKD';
+  static const String baseUrlApi = '';
+  static const String googleClientId = '';
+  static const String appleClientId = '';
+  static const String authAppleClientId = '';
   static const String uriApple = 'https://verbena-sudsy-enigmosaurus.glitch.me/callbacks/sign_in_with_apple';
   static const String privacyPolicy = "https://linguo.me/privacy-policy";
   static const String termsOfUse = "https://linguo.me/terms-of-use";
@@ -12,5 +12,5 @@ class AppConfig {
   static const String assetsPremiumIcons = "assets/premium_icons/";
   static const String urlApp = "play.google.com/store/apps/details?id=me.linguo.mobile";
   static const String instanceName = "LinguoMe Mobile";
-  static const String amplitudeApiKey = "1ad97731c6ecc4430159cc85ea4a7935";
+  static const String amplitudeApiKey = "";
 }
